d0cbda3b376a2cb5670f834ced771b5bcd8bc7e2
Sudhanshu <sudyal101@gmail.com>
2023-07-25 19:24:07 +0530

    fix routes

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 require('dotenv').config();
-const port = process.env.PORT;
-// const port = 8000;
+// const port = process.env.PORT;
+const port = 8000;
 const app = express();
 
-console.log(process.env.SECERET);
+// console.log(process.env.SECERET);
 
 app.get('/', (req, res)=>{
-    return res.end('Test file version-2');
+    return res.end('Test file version-pro');
 })
 
 app.listen(port, function(err) {
